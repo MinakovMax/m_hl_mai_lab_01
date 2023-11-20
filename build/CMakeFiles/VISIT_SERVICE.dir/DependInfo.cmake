6aae5182f52aaa0e6810cd656124d2e427017ab8
Minakov <minakov86@yandex.ru>
2023-11-20 14:38:52 +0300

    v09

---
+++ build/CMakeFiles/VISIT_SERVICE.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/minakov86/m_hl_mai_lab_01/config/config.cpp" "/home/minakov86/m_hl_mai_lab_01/build/CMakeFiles/VISIT_SERVICE.dir/config/config.cpp.o"
+  "/home/minakov86/m_hl_mai_lab_01/database/database.cpp" "/home/minakov86/m_hl_mai_lab_01/build/CMakeFiles/VISIT_SERVICE.dir/database/database.cpp.o"
+  "/home/minakov86/m_hl_mai_lab_01/database/user.cpp" "/home/minakov86/m_hl_mai_lab_01/build/CMakeFiles/VISIT_SERVICE.dir/database/user.cpp.o"
+  "/home/minakov86/m_hl_mai_lab_01/database/visit.cpp" "/home/minakov86/m_hl_mai_lab_01/build/CMakeFiles/VISIT_SERVICE.dir/database/visit.cpp.o"
+  "/home/minakov86/m_hl_mai_lab_01/visit_main.cpp" "/home/minakov86/m_hl_mai_lab_01/build/CMakeFiles/VISIT_SERVICE.dir/visit_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "POCO_ENABLE_CPP11"
+  "POCO_ENABLE_CPP14"
+  "POCO_HAVE_FD_EPOLL"
+  "POCO_OS_FAMILY_UNIX"
+  "XML_DTD"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE64_SOURCE"
+  "_REENTRANT"
+  "_THREAD_SAFE"
+  "_XOPEN_SOURCE=500"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/mysql"
+  "/usr/local/include/mysql"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
